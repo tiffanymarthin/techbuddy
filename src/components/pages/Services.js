0dff0a9fc6b963f331d88d6b0e276934cd20ea86
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-02-20 16:15:34 -0800

    Modified the landing page directories

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import '../../App.css';
+import React from "react";
+
+import Cards from "../Cards";
+import "../../App.css";
 
 export default function Services() {
-  return <h1 className='services'>SERVICES</h1>;
+  return (
+    <>
+      <h1 className="services">SERVICES</h1>
+      <Cards />
+    </>
+  );
 }
