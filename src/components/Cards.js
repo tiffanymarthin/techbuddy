09e6c18771c1011ceffe4f2a14d196f1100cf0e6
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-02-20 21:08:56 -0800

    tutorial page gallery is done

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -33,19 +33,62 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="images/img-1.png"
-              label="Youtube: Did you know?"
+              src="images/spotify-1.png"
+              label="Getting Started Spotify"
               path="/services"
             />
             <CardItem
-              src="images/img-2.png"
-              // text="Experience Football on Top of the Himilayan Mountains"
-              label="Youtube: 5 Basic Settings"
+              src="images/spotify-2.png"
+              label="Listen while Driving"
               path="/products"
             />
             <CardItem
-              src="images/img-3.png"
-              label="Google: Things you should l.."
+              src="images/spotify-3.png"
+              label="How to create a playlist"
+              path="/sign-up"
+            />
+          </ul>
+        </div>
+      </div>
+      <h1>Google Meets</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="images/google-1.png"
+              // label="How to start a Meeting"
+              path="/services"
+            />
+            <CardItem
+              src="images/google-2.png"
+              // label="Tips for Google Meets"
+              path="/products"
+            />
+            <CardItem
+              src="images/google-3.png"
+              // label="Tools that they have"
+              path="/sign-up"
+            />
+          </ul>
+        </div>
+      </div>
+      <h1>Zoom</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="images/zoom-1.png"
+              // label="Basic Features for Beginners"
+              path="/services"
+            />
+            <CardItem
+              src="images/zoom-2.png"
+              // label="Download Zoom"
+              path="/products"
+            />
+            <CardItem
+              src="images/zoom-3.png"
+              // label="Some useful Shortcuts"
               path="/sign-up"
             />
           </ul>
