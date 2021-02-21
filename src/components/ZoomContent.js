56e781414d4d786fd9b68789b1718866637d2999
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-02-20 21:47:33 -0800

    zoom tutorial content

---
+++ src/components/ZoomContent.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./ZoomContent.css";
+
+function ZoomContent() {
+  return (
+    <div className="zoomcontent-container">
+      <h2>
+        New to Zoom?
+        <br />
+        Here are features that you are going to be using A LOT.
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt
+        faucibus adipiscing varius viverra molestie tortor. Sed fermentum purus
+        at suspendisse cras at tellus in. Pharetra justo, nulla mattis dui. In
+        platea at posuere ut imperdiet a, nibh duis. Consectetur cras nam purus
+        nisi. Nunc nunc, vulputate cursus eu, libero ut massa bibendum. Sit
+        nulla id morbi pharetra a vel. Aenean bibendum consectetur urna tortor.
+        Ut suscipit in viverra at condimentum felis ut nunc morbi.
+      </p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <p>
+        Let’s take a look at the screen you will see once you get into a Zoom
+        call.
+      </p>
+      <img src="/images/zc-1.png"></img>
+      <br />
+      <br />
+      <p>
+        You probably wonder that what these buttons actually do? Don’t worry, I
+        will explain what they mean and how do you use it step by step.
+      </p>
+      <br />
+      <br />
+      <img src="/images/zc-2.png"></img>
+      <br />
+      <br />
+      <p>
+        This button can control your microphone within Zoom. Unlike in-person
+        conversation, it’s a good manner to mute yourself when you are not
+        talking/speaking so that other people in the room can hear clearly what
+        the speaker is saying without any distraction. Don’t worry. The way you
+        Mute/ Unmute yourself is pretty easy! All you have to do is click on it.
+        Once you click on the button, the red crossed line will appear on top of
+        the microphone icon. It means that you’re muted so no one can hear you
+        even though you’re talking. Make sure to turn it back on before you
+        speak!! The buttons will look like these :
+      </p>
+    </div>
+  );
+}
+
+export default ZoomContent;
--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../App.css";
 import Zoom from "../Zoom";
+import ZoomContent from "../ZoomContent";
 
 function Products() {
   return (
     <>
       <Zoom />
+      <ZoomContent />
     </>
   );
 }
