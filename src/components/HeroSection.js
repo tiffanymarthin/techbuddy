aee28f8c6981f6b9c3e9bffefb9c7d21140757c7
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-02-20 17:04:24 -0800

    landing page top part

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "../App.css";
-import { Button } from "./Button";
 import "./HeroSection.css";
 
 function HeroSection() {
   return (
     <div className="hero-container">
-      <h1>Learn the latest technologies at Tech Buddy</h1>
-      <p>What are you waiting for?</p>
+      <div class="hero-links">
+        <div className="hero-link-wrapper">
+          <div class="hero-link-items">
+            <h2>Learn the latest technologies at Tech Buddy</h2>
+            <p>What are you waiting for?</p>
+          </div>
+        </div>
+        <div className="hero-link-wrapper">
+          <div class="hero-link-items">
+            <h1>Learn the latest technologies at Tech Buddy</h1>
+            <p>What are you waiting for?</p>
+          </div>
+        </div>
+      </div>
       <div className="hero-btns"></div>
     </div>
   );
