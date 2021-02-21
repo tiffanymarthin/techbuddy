0cd1fd7b0c919c0e03ccec12c6091aa47442e593
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-02-20 18:19:13 -0800

    added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,12 +29,12 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            TechBuddy
-            <i class="fab fa-typo3" />
+            Tech Buddy
+            <i class="fas fa-project-diagram" />
           </Link>
-          <div className="menu-icon" onClick={handleClick}>
+          {/* <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
-          </div>
+          </div> */}
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
